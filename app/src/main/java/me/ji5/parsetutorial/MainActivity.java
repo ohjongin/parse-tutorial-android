715a9e5524bb5e2c4c7e08ae6723caf429bd39d5
yeojoy <yeojoy.kim@gmail.com>
2015-07-22 14:40:20 +0900

    manifest에 남겨진 필요없는 code 제거

--- app/src/main/java/me/ji5/parsetutorial/MainActivity.java
+++ app/src/main/java/me/ji5/parsetutorial/MainActivity.java
@@ -69,6 +69,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hideTextViews();
 
         switch (v.getId()) {
+            case R.id.btn_signup:
+                onSignUp();
+                break;
+            case R.id.btn_login:
+                onLogin();
+                break;
             case R.id.btn_upload_object:
                 onUploadObject();
                 break;
